5fdcc24f72cfaf191f78bd6d7e6401c858a6197c
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-07 13:32:58 +0200

    Initial Commit

---
+++ GreenGarden/GreenGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenGardenApp.swift
+//  GreenGarden
+//
+//  Created by Eric on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GreenGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
