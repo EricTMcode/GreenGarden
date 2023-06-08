948a92cb8dcb8b02054617af2d887cd399a72c2e
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-08 14:39:12 +0200

    - Add static example

---
+++ GreenGarden/Screens/VegetableDetailScreen.swift
@@ -0,0 +1,22 @@
+//
+//  VegetableDetailScreen.swift
+//  GreenGarden
+//
+//  Created by Eric on 07/06/2023.
+//
+
+import SwiftUI
+
+struct VegetableDetailScreen: View {
+    let vegetable: VegetableViewModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VegetableDetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        VegetableDetailScreen(vegetable: <#VegetableViewModel#>)
+    }
+}
